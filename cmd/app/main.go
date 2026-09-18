c1d7de75f53dcf161ff0231f40e8df54ef5b52ee
WindowHalberdier <isamaraxixa4542@gmail.com>
2026-09-18 12:48:04 +0200

    Import repository files

---
+++ cmd/app/main.go
@@ -0,0 +1,14 @@
+// Build: 2c02fc884d2bfb1b4599db82473fcedc
+package main
+
+import "fmt"
+
+func clamp(value, minimum, maximum int) int {
+	if value < minimum { return minimum }
+	if value > maximum { return maximum }
+	return value
+}
+
+func main() {
+	fmt.Println(clamp(12, 0, 10))
+}
